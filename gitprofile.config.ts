9823fd69b9ab77115f5966bcc552cea0d10a7e24
FlanChan <104259619+FlanChanXwO@users.noreply.github.com>
2026-02-06 14:34:59 +0800

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -2,29 +2,45 @@ const config = {
   github: {
     username: 'FlanChanXwO', 
   },
+  seo: {
+    title: 'Portfolio of FlanChanXwO',
+    description: '',
+    image: '',
+  },
   social: {
     gitee: 'https://gitee.com/flanchanowo',
     bilibili: 'https://space.bilibili.com/184855403',
     steam: 'https://steamcommunity.com/id/Flan_owo'
+  },
+  resume: {
+    fileUrl: '', 
   },
   skills: [
     'Python', 'Java', 'TypeScript', 'MySQL', 'Docker'
   ],
   projects: {
     github: {
-      display: true, // 开启自动展示
+      display: true, 
       header: 'My Projects',
       mode: 'automatic',
       limit: 10,
       exclude: {
-        forks: false, // 是否排除fork的项目
-        projects: [], // 排除特定的项目
+        forks: false, 
+        projects: [], 
       },
     },
+    external: {
+      header: 'My Projects',
+      projects: [], // 外部项目留空即可
+    },
   },
+  footer: `Made with ❤️`,
   themeConfig: {
     defaultTheme: 'dark',
-  }
-}
+    respectPrefersColorScheme: false,
+    displayAppBarHighlight: true,
+    fontFamily: 'sans-serif',
+  },
+};
 
 export default config;
